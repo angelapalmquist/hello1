914e1abc917002a6781462a0ea7d1911c23d2f8a
Angela Palmquist <apalmquist3@huskers.unl.edu>
2020-08-23 00:41:42 +0000

    initial commit

---
+++ hello1.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Angela Palmquist
+ *         apalmquist3@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Angela Palmquist\n");
+
+  return 0;
+}
